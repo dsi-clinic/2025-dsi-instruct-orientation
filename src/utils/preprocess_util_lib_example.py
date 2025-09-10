027e9fe3edbc9f69816094710d92077e37564d42
Trevor Spreadbury <trevorspreadbury@gmail.com>
2025-09-10 17:08:52 +0000

    update paths

--- src/utils/preprocess_util_lib_example.py
+++ src/utils/preprocess_util_lib_example.py
@@ -11,15 +11,14 @@ current_directory = Path(__file__).parent
 REPO_ROOT = current_directory.parent.parent
 
 
-def save_random_dataframe(output_directory: Path, output_file: Path) -> None:
+def save_random_dataframe(output_path: Path | str) -> None:
     """Creates a random dataframe and saves to csv
 
     Args:
-        output_directory: absolute path to directory to save df in
-        output_file: filename to save dataframe to in output_directory
+        output_path: absolute path to save df in
     Returns: None.
     """
     random_df = pd.DataFrame(
         np.random.randint(0, 100, size=(100, 4)), columns=list("ABCD")
     )
-    random_df.to_csv(output_directory / output_file)
+    random_df.to_csv(output_path)
